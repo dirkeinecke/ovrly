050dff2499ab449ac2629b94d65b603fbffdb125
Dirk Einecke <dirkeinecke@users.noreply.github.com>
2019-01-28 08:49:47 +0100

    Namespace with Object Literal Notation
    closes #5

--- dist/js/ovrly.js
+++ dist/js/ovrly.js
@@ -10,80 +10,84 @@
  * https://github.com/dirkeinecke/ovrly/blob/master/LICENSE
  *
  */
- 
-function ovrly(link) {
-  const yOffset = 0;
 
-  let overlay = document.createElement('div');
-  overlay.setAttribute('id','ovrly');
-  overlay.setAttribute('data-body-overflow', document.body.style.overflow);
-  overlay.setAttribute(
-    'style',
-    'position:fixed;z-index:100;top:'+yOffset+'px;left:0;width:100%;height:100%;background-color:rgba(0,0,0,.5);text-align:center;box-sizing:border-box;padding:50px;'
-  );
-  let image = document.createElement('img');
-  image.setAttribute('src',link.href);
-  image.setAttribute(
-    'style',
-    'width:auto;max-width:100%;height:auto;max-height:100%;margin:auto;position:relative;top:50%;transform:perspective(1px) translateY(-50%);'
-  );
-  overlay.appendChild(image);
+var ovrly = {
 
-  let closeButton = document.createElement('a');
-  closeButton.setAttribute('href','#');
-  closeButton.setAttribute('onclick', 'return ovrlyClose({\'key\':\'Escape\'});');
-  closeButton.setAttribute(
-    'style',
-    'position:absolute;z-index:101;top:0;right:0;text-decoration:none;color:#fff;font-size:30px;padding:10px 15px 10px 10px;'
-  );
-  const closeButtonText = document.createTextNode('×');
-  closeButton.appendChild(closeButtonText);
-  overlay.appendChild(closeButton);
-
-  document.body.appendChild(overlay);
-  document.body.style.overflow = 'hidden';
-
-  document.addEventListener(
-    'keydown',
-    ovrlyClose
-  );
-
-  window.addEventListener(
-    'scroll',
-    ovrlyClose
-  );
-
-  return false;
-}
-
-function ovrlyClose(event) {
-  var fire = false;
-
-  if ('key' in event) {
-    if (event.key == 'Escape' || event.key == 'Esc') {
-      fire = true;
-    }
-  } else if ('type' in event) {
-    if (event.type == 'scroll') {
-      fire = true;
-    }
-  }
-
-  if (fire == true) {
-    const element = document.getElementById('ovrly');
-    document.body.style.overflow = element.getAttribute('data-body-overflow');
-    element.parentNode.removeChild(element);
-
-    document.removeEventListener(
+  open: function(link) {
+    const yOffset = 0;
+  
+    let overlay = document.createElement('div');
+    overlay.setAttribute('id','ovrly');
+    overlay.setAttribute('data-body-overflow', document.body.style.overflow);
+    overlay.setAttribute(
+      'style',
+      'position:fixed;z-index:100;top:'+yOffset+'px;left:0;width:100%;height:100%;background-color:rgba(0,0,0,.5);text-align:center;box-sizing:border-box;padding:50px;'
+    );
+    let image = document.createElement('img');
+    image.setAttribute('src',link.href);
+    image.setAttribute(
+      'style',
+      'width:auto;max-width:100%;height:auto;max-height:100%;margin:auto;position:relative;top:50%;transform:perspective(1px) translateY(-50%);'
+    );
+    overlay.appendChild(image);
+  
+    let closeButton = document.createElement('a');
+    closeButton.setAttribute('href','#');
+    closeButton.setAttribute('onclick', 'return ovrly.close({\'key\':\'Escape\'});');
+    closeButton.setAttribute(
+      'style',
+      'position:absolute;z-index:101;top:0;right:0;text-decoration:none;color:#fff;font-size:30px;padding:10px 15px 10px 10px;'
+    );
+    const closeButtonText = document.createTextNode('×');
+    closeButton.appendChild(closeButtonText);
+    overlay.appendChild(closeButton);
+  
+    document.body.appendChild(overlay);
+    document.body.style.overflow = 'hidden';
+  
+    document.addEventListener(
       'keydown',
-      ovrlyClose
+      ovrly.close
     );
-
-    window.removeEventListener(
+  
+    window.addEventListener(
       'scroll',
-      ovrlyClose
+      ovrly.close
     );
+  
+    return false;
+  },
+  
+  close: function(event) {
+    var fire = false;
+  
+    if ('key' in event) {
+      if (event.key == 'Escape' || event.key == 'Esc') {
+        fire = true;
+      }
+    } else if ('type' in event) {
+      if (event.type == 'scroll') {
+        fire = true;
+      }
+    }
+  
+    if (fire == true) {
+      const element = document.getElementById('ovrly');
+      document.body.style.overflow = element.getAttribute('data-body-overflow');
+      element.parentNode.removeChild(element);
+  
+      document.removeEventListener(
+        'keydown',
+        ovrly.close
+      );
+  
+      window.removeEventListener(
+        'scroll',
+        ovrly.close
+      );
+    }
+  
+    return false;
   }
 
-  return false;
 }
